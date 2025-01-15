4112e981199e4807492af8344ff822da624461d5
Omar Baabouj <baabouj.dev@gmail.com>
2025-01-16 00:14:21 +0100

    fix(frontend): use env variable for api url

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -1,6 +1,6 @@
 import Axios from "axios";
 
 export const axios = Axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true, // Include cookies in requests
 });
